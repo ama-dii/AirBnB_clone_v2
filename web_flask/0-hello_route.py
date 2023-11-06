5fd0c2de64ef0be382828910ab6e81425ea41c5f
Amadi Glory <whytes2256@gmail.com>
2023-11-06 00:36:51 -0800

    0-hello_route.py

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -5,11 +5,13 @@ Import flask
 from flask import Flask
 app = Flask(__name__)
 
-
+# Define the rout for the roo URL '/'
 @app.route('/', strict_slashes=False)
-def Hello_HBNB():
+def hello_hbnb():
     return 'Hello HBNB!'
 
 
 if __name__ == '__main__':
+    # start flask development server
+    # listen on all available network interface (0.0.0.0) and port 5000
     app.run(host='0.0.0.0', port=5000, debug=True)
